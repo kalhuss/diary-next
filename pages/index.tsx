4fed003aba13a73acd6061cfee25e40fcc3b15e4
Kalim <kkh10@aber.ac.uk>
2022-11-28 00:12:31 +0000

    Larger title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className='text-indigo-500'>Diary</h1>
+      <h1 className='text-indigo-500 text-6xl text-center'>Diary</h1>
 
     </div>
   )
